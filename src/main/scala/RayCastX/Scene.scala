a5ab78e752a22ae05ee31f600416a777bf5a7fdb
Jeffrey Drake <drakej@uwindsor.ca>
2013-12-27 03:38:17 -0500

    Modified colours, sphere now renders

--- src/main/scala/RayCastX/Scene.scala
+++ src/main/scala/RayCastX/Scene.scala
@@ -11,6 +11,7 @@ package object RayCastX {
 
 */
 
+import scala.math.{acos}
 import ps.tricerato.pureimage._
 import breeze.linalg._
 
@@ -18,9 +19,10 @@ case class Scene(width : Int, height : Int, obj : IntersectionTest) extends Imag
   def apply(x : Int, y : Int) : RGB = {
     val fx = x.toDouble / width.toDouble
     val fy = y.toDouble / height.toDouble
+    def thetamod(angle : Double) = (angle - acos(-1)) / acos(-1)
 
     obj.intersection(Ray(DenseVector(fx, fy, 0.0), DenseVector(0.0, 0.0, 1.0))) match {
-      case Some(a) => RGB.fromChannels((a.theta * 255.0).toInt, (a.theta * 255.0).toInt, (a.theta * 255.0).toInt)
+      case Some(a) => RGB.fromChannels((thetamod(a.theta) * 255.0).toInt, (thetamod(a.theta) * 255.0).toInt, (thetamod(a.theta) * 255.0).toInt)
       case None => RGB.fromChannels(0, 0, 0)
     }
   }
